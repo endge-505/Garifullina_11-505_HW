d88f5b9248b515f42c97b8e5193f154f5ab62b8c
endge-505 <endgegr2@gmail.com>
2025-12-14 23:44:27 +0300

    Delete SR directory

--- SR/src/OrderSaved.java
+++
@@ -1,6 +0,0 @@
-public class OrderSaved implements  Print{
-    public void print(String print) {
-        System.out.println("I can only output information about " + print);
-    }
-
-}
--- SR/src/Print.java
+++
@@ -1,3 +0,0 @@
-public interface Print {
-    void print(String print);
-}
