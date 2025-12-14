99b7c4cbe5cef60494d0eff5636ae76f5b5619ce
endge-505 <endgegr2@gmail.com>
2025-12-14 23:44:04 +0300

    Delete LSP directory

--- LSP/src/Pinguin.java
+++
@@ -1,5 +0,0 @@
-public class Pinguin extends Bird{
-    public void eat(){
-        System.out.println("Пингвин кушает. ");
-    }
-}
--- LSP/src/Sparrowl.java
+++
@@ -1,8 +0,0 @@
-public class Sparrowl extends Bird implements Fly{
-    public void eat(){
-        System.out.println("Воробей кушает.");
-    }
-    public void fly(){
-        System.out.println("Воробей летает.");
-    }
-}
